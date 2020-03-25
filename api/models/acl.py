31657c400a15d5072ca969b7220696a79c9f444e
Administrator <admin@example.com>
2020-03-25 19:45:05 +0400

    login

--- api/models/acl.py
+++ api/models/acl.py
@@ -131,50 +131,50 @@ class RoleRelation(Model):
     parent_id = db.Column(db.Integer, db.ForeignKey('acl_roles.id'))
     child_id = db.Column(db.Integer, db.ForeignKey('acl_roles.id'))
 
-# class ResourceType(Model):
-#     __tablename__ = "acl_resource_types"
+class ResourceType(Model):
+    __tablename__ = "acl_resource_types"
 
-#     name = db.Column(db.String(64), index=True)
-#     description = db.Column(db.Text)
-#     app_id = db.Column(db.Integer, db.ForeignKey('acl_apps.id'))
+    name = db.Column(db.String(64), index=True)
+    description = db.Column(db.Text)
+    app_id = db.Column(db.Integer, db.ForeignKey('acl_apps.id'))
 
-# class ResourceGroup(Model):
-#     __tablename__ = "acl_resource_groups"
+class ResourceGroup(Model):
+    __tablename__ = "acl_resource_groups"
 
-#     name = db.Column(db.String(64), index=True, nullable=False)
-#     resource_type_id = db.Column(db.Integer, db.ForeignKey("acl_resource_types.id"))
+    name = db.Column(db.String(64), index=True, nullable=False)
+    resource_type_id = db.Column(db.Integer, db.ForeignKey("acl_resource_types.id"))
 
-#     app_id = db.Column(db.Integer, db.ForeignKey('acl_apps.id'))
+    app_id = db.Column(db.Integer, db.ForeignKey('acl_apps.id'))
 
-# class Resource(Model):
-#     __tablename__ = "acl_resources"
+class Resource(Model):
+    __tablename__ = "acl_resources"
 
-#     name = db.Column(db.String(128), nullable=False)
-#     resource_type_id = db.Column(db.Integer, db.ForeignKey("acl_resource_types.id"))
+    name = db.Column(db.String(128), nullable=False)
+    resource_type_id = db.Column(db.Integer, db.ForeignKey("acl_resource_types.id"))
 
-#     app_id = db.Column(db.Integer, db.ForeignKey("acl_apps.id"))
+    app_id = db.Column(db.Integer, db.ForeignKey("acl_apps.id"))
 
-# class ResourceGroupItems(Model):
-#     __tablename__ = "acl_resource_group_items"
+class ResourceGroupItems(Model):
+    __tablename__ = "acl_resource_group_items"
 
-#     group_id = db.Column(db.Integer, db.ForeignKey('acl_resource_groups.id'), nullable=False)
-#     resource_id = db.Column(db.Integer, db.ForeignKey('acl_resources.id'), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('acl_resource_groups.id'), nullable=False)
+    resource_id = db.Column(db.Integer, db.ForeignKey('acl_resources.id'), nullable=False)
 
-# class Permission(Model):
-#     __tablename__ = "acl_permissions"
+class Permission(Model):
+    __tablename__ = "acl_permissions"
 
-#     name = db.Column(db.String(64), nullable=False)
-#     resource_type_id = db.Column(db.Integer, db.ForeignKey("acl_resource_types.id"))
+    name = db.Column(db.String(64), nullable=False)
+    resource_type_id = db.Column(db.Integer, db.ForeignKey("acl_resource_types.id"))
 
-#     app_id = db.Column(db.Integer, db.ForeignKey("acl_apps.id"))
+    app_id = db.Column(db.Integer, db.ForeignKey("acl_apps.id"))
 
-# class RolePermission(Model):
-#     __tablename__ = "acl_role_permissions"
+class RolePermission(Model):
+    __tablename__ = "acl_role_permissions"
 
-#     rid = db.Column(db.Integer, db.ForeignKey('acl_roles.id'))
-#     resource_id = db.Column(db.Integer, db.ForeignKey('acl_resources.id'))
-#     group_id = db.Column(db.Integer, db.ForeignKey('acl_resource_groups.id'))
-#     perm_id = db.Column(db.Integer, db.ForeignKey('acl_permissions.id'))
+    rid = db.Column(db.Integer, db.ForeignKey('acl_roles.id'))
+    resource_id = db.Column(db.Integer, db.ForeignKey('acl_resources.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('acl_resource_groups.id'))
+    perm_id = db.Column(db.Integer, db.ForeignKey('acl_permissions.id'))
 
-#     perm = db.relationship("Permission", backref='acl_role_permissions.perm_id')
+    perm = db.relationship("Permission", backref='acl_role_permissions.perm_id')
 
