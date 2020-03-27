9dc23a2d157cbe4c7bde7b4f82d4f18dc5d1d8f6
Administrator <admin@example.com>
2020-03-27 16:15:07 +0400

    3

--- api/views/acl/user.py
+++ api/views/acl/user.py
@@ -4,3 +4,16 @@ from flask import request
 from flask import session
 from flask_login import current_user
 
+class GetUserInfoView(APIView):
+    url_prefix = "/users/info"
+
+    def get(self):
+        print(session.get("CAS_USERNAME"),current_user.nickname,'usersinfo name')
+        print(session.get("acl", {}).get("parentRoles", []),'permissions=1')
+        name = session.get("CAS_USERNAME") or current_user.nickname
+        role = dict(permissions=session.get("acl", {}).get("parentRoles", []))
+        avatar = current_user.avatar
+
+        return self.jsonify(result=dict(name=name,
+                                        role=role,
+                                        avatar=avatar))
