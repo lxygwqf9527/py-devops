868c8d0f75bef7320b25a4c6aaefd707dad96a04
Administrator <admin@example.com>
2020-03-27 12:10:39 +0400

    login

--- api/lib/decorator.py
+++ api/lib/decorator.py
@@ -24,6 +24,7 @@ def args_required(*required_args):
         @wraps(func)
         def wrapper(*args, **kwargs):
             for arg in required_args:
+                print(required_args,arg)
                 if arg not in request.values:
                     return abort(400, "Argument <{0}> is required".format(arg))
             return func(*args, **kwargs)
