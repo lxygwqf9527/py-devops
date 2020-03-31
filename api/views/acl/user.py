fbd67e74c0cb25f124e87e26b72e83a5608b365b
Administrator <admin@example.com>
2020-03-31 10:27:11 +0400

    3

--- api/views/acl/user.py
+++ api/views/acl/user.py
@@ -9,12 +9,10 @@ class GetUserInfoView(APIView):
     url_prefix = "/users/info"
 
     def get(self):
-        print(session.get("CAS_USERNAME"),current_user.nickname,'usersinfo name')
-        print(session.get("acl", {}).get("parentRoles", []),'permissions=1')
         name = session.get("CAS_USERNAME") or current_user.nickname
         role = dict(roles=session.get("acl", {}).get("parentRoles", []))
-        print(role)
         avatar = current_user.avatar
+        
         return self.jsonify(result=dict(name=name,
                                         role=role,
                                         avatar=avatar))
