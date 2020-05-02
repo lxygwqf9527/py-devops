819117f19de147567d47ec00df43a5237c2b0ea3
Administrator <admin@example.com>
2020-05-02 15:54:48 +0400

    3

--- api/views/account.py
+++ api/views/account.py
@@ -13,7 +13,7 @@ from api.lib.decorator import args_required
 from api.lib.perm.auth import auth_abandoned
 from api.models.account import User, Role
 from api.resource import APIView
-from api.lib.perm.acl.role import RoleRelationCRUD
+# from api.lib.perm.acl.role import RoleRelationCRUD
 # from api.lib.perm.acl.cache import RoleCache
 
 class LoginView(APIView):
