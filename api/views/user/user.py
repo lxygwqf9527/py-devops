6a882c6a52ad11cf8c46fb7d0eeefc56ec8afe62
Administrator <admin@example.com>
2020-05-08 14:46:01 +0400

    导入修改4

--- api/views/user/user.py
+++ api/views/user/user.py
@@ -1,8 +1,7 @@
 # -*- coding:utf-8 -*-
 
-from flask import request
-from flask import session
-from flask_login import current_user, abort
+from flask import request, abort, session
+from flask_login import current_user
 from api.resource import APIView
 
 class GetUserInfoView(APIView):
