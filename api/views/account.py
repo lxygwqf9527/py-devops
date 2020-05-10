3f0fe62fc9629a5859c59a0eb7a9598d96c34c45
Administrator <admin@example.com>
2020-05-10 23:15:00 +0400

    3

--- api/views/account.py
+++ api/views/account.py
@@ -72,12 +72,12 @@ class LoginView(APIView):
                                 last_login=last_login,last_ip=last_ip)
         login_user(user)
         
-        return self.jsonify(
-            access_token= user.access_token,
+        return self.jsonify({
+            access_token = user.access_token,
             nickname=user.nickname,
             is_supper=user.is_supper,
             has_real_ip=True if x_real_ip else False,
-            permissions=[] if user.is_supper else user.page_perms
+            permissions=[] if user.is_supper else user.page_perms}
     )
 
 class LogoutView(APIView):
