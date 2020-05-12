15d5250ab14ed5dfdfa4aa8987f869ff9c9f03ec
Administrator <admin@example.com>
2020-05-12 15:43:52 +0400

    登录完成

--- api/models/account.py
+++ api/models/account.py
@@ -108,6 +108,7 @@ class User(Model):
 
     @property
     def page_perms(self):
+        print(self.role)
         if self.role and self.role.page_perms:
             data = []
             perms = json.loads(self.role.page_perms)
