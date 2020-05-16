8cde5d207a09bd3ef729221c30cfe0220f9636c3
Administrator <admin@example.com>
2020-05-16 14:31:01 +0400

    notify第6次提交

--- api/libs/cache/user.py
+++ api/libs/cache/user.py
@@ -1,5 +1,5 @@
 from api.extensions import cache
-from api.models.account import User, Role,  Notify
+from api.models.account import User, Role
 
 class UserCache(object):
     '''
@@ -83,17 +83,17 @@ class PermissionCache(object):
     def set(cls, id, permission):
         cache.set(cls.PREFIX_ID.format(id),permission)
         
-class NotifyCache(object):
-    '''
-        通知相关的缓存类
-    '''
-    PREFIX_QUIET = "Notify:quiet"
+# class NotifyCache(object):
+#     '''
+#         通知相关的缓存类
+#     '''
+#     PREFIX_QUIET = "Notify:quiet"
 
-    @classmethod
-    def set_by_time(cls, time):
-        cache.set(cls.PREFIX_QUIET, time)
+#     @classmethod
+#     def set_by_time(cls, time):
+#         cache.set(cls.PREFIX_QUIET, time)
     
-    @classmethod
-    def get_by_time(cls, time):
-        return cache.get(cls.PREFIX_QUIET)
+#     @classmethod
+#     def get_by_time(cls, time):
+#         return cache.get(cls.PREFIX_QUIET)
 
--- api/models/account.py
+++ api/models/account.py
@@ -12,7 +12,7 @@ from flask_sqlalchemy import BaseQuery
 from api.extensions import db
 from api.libs import Model, CRUDModel
 from api.libs.utils import human_datetime
-from api.libs.cache.user import NotifyCache
+# from api.libs.cache.user import NotifyCache
 
 
 
@@ -157,29 +157,29 @@ class Role(CRUDModel):
         pass
 
 
-class Notify(Model):
-    TYPES = (
-        ('1', '通知'),
-        ('2', '待办'),
-    )
-    SOURCES = (
-        ('monitor', '监控中心'),
-        ('schedule', '任务计划'),
-    )
-    title = db.Column(db.String(255))
-    source = db.Column(db.String(10), choices=SOURCES)
-    type = db.Column(db.String(2), choices=TYPES)
-    content = db.Column(db.String(255), nullable=True)
-    unread = db.Column(db.Boolean,default=True)
-    link = db.Column(db.Boolean, nullable=True)
+# class Notify(Model):
+#     TYPES = (
+#         ('1', '通知'),
+#         ('2', '待办'),
+#     )
+#     SOURCES = (
+#         ('monitor', '监控中心'),
+#         ('schedule', '任务计划'),
+#     )
+#     title = db.Column(db.String(255))
+#     source = db.Column(db.String(10), choices=SOURCES)
+#     type = db.Column(db.String(2), choices=TYPES)
+#     content = db.Column(db.String(255), nullable=True)
+#     unread = db.Column(db.Boolean,default=True)
+#     link = db.Column(db.Boolean, nullable=True)
 
-    created_at = db.Column(db.string(20), default=human_datetime)
+#     created_at = db.Column(db.string(20), default=human_datetime)
 
-    @classmethod
-    def make_notify(cls, source, type, title, content=None, with_quiet=True):
-        if not with_quiet or time.time() - NotifyCache.get_by_time() > 3600:
-            NotifyCache.set_by_time(time.time())
-            self.create(source=source, title=title, type=type, content=content)
+#     @classmethod
+#     def make_notify(cls, source, type, title, content=None, with_quiet=True):
+#         if not with_quiet or time.time() - NotifyCache.get_by_time() > 3600:
+#             NotifyCache.set_by_time(time.time())
+#             self.create(source=source, title=title, type=type, content=content)
 
-    def __str__(self):
-        return '<Notify %r>' % self.title
+#     def __str__(self):
+#         return '<Notify %r>' % self.title
