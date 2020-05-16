6fc9afbf77f1b8d570c441aa1820e68e9302a506
Administrator <admin@example.com>
2020-05-16 12:02:44 +0400

    notify第2次提交

--- api/libs/cache/user.py
+++ api/libs/cache/user.py
@@ -1,6 +1,5 @@
 from api.extensions import cache
-from api.models.account import User
-from api.mod
+from api.models.account import User,Notify
 
 class UserCache(object):
     '''
@@ -77,3 +76,18 @@ class PermissionCache(object):
                      cache.get(cls.PREFIX_NICKNAME.format(key))
 
         if not permission:
+
+class NotifyCache(object):
+    '''
+        通知相关的缓存类
+    '''
+    PREFIX_QUIET = "Notify:quiet"
+
+    @classmethod
+    def set_by_time(cls, time):
+        cache.set(cls.PREFIX_QUIET, time)
+    
+    @classmethod
+    def get_by_time(cls, time):
+        return cache.get(cls.PREFIX_QUIET)
+
