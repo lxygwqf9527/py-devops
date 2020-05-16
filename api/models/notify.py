3502d1fb3dd979cd55d58725ee80431bab4e4d5d
Administrator <admin@example.com>
2020-05-16 16:04:40 +0400

    notify第12次提交

--- api/models/notify.py
+++ api/models/notify.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from api.extensions import db
-from api.libs import Model
+from api.libs import Model,human_datetime
 from api.libs.cache.notify import NotifyCache
 
 class Notify(Model):
