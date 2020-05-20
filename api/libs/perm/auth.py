0cd9cd51fc74889fb5e2b251a72728ed8d5f1301
Administrator <admin@example.com>
2020-05-20 13:51:09 +0400

    notify第36次提交

--- api/libs/perm/auth.py
+++ api/libs/perm/auth.py
@@ -16,6 +16,7 @@ from api.models.account import User
 from api.libs.cache import UserCache
 
 def _auth_with_session():
+    print(user)
     if isinstance(getattr(g, 'user', None), User):
         login_user(g.user)
         return True
