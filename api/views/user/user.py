dfd27f2b1dc4c9d3185e5fbfce5d40093546ec8d
Administrator <admin@example.com>
2020-05-21 21:27:45 +0400

    self第一次修改

--- api/views/user/user.py
+++ api/views/user/user.py
@@ -19,4 +19,18 @@ class GetUserInfoView(APIView):
 
 class SelfView(APIView):
     url_prefix = "/self"
-    pass
+    
+    def patch(self):
+        old_password = request.values.get('old_password', None)
+        new_password = request.values.get('new_password', None)
+        if len(new_password) < 6:
+            return self.jsonify(error='请设置至少6位的新密码')
+        print(g.user)
+        if g.user.check_password(old_password):
+            print('叼你妈的')
+            
+        else:
+            return self.jsonify(error='原密码错误，请重新输入')
+    
+            
+
