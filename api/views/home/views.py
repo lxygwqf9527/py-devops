8a6d5c7f4c12de24ea771fcc3342ca6761ea1cd2
Administrator <admin@example.com>
2020-06-02 16:13:53 +0400

    home get deploy5

--- api/views/home/views.py
+++ api/views/home/views.py
@@ -2,10 +2,11 @@
 from datetime import datetime, timedelta
 from flask import request
 from sqlalchemy import and_
+import json
 
 from api.libs import human_date
 from api.resource import APIView
-from api.models import App, Host, Task, Detection, Alarm, DeployRequest
+from api.models import App, Host, Task, Detection, Alarm, DeployRequest, Deploy
 
 class GetStatistic(APIView):
     '''
@@ -45,11 +46,24 @@ class GetRequest(APIView):
         deploy发布展示
     '''
     url_prefix = '/request'
+
     def get(self):
         data = { x.id: {'name': x.name, 'count': 0 } for x in App.query.all() }
-        print(data,'=-=-=-=-=-==-=-=-=-=-')
         for req in DeployRequest.query.filter(DeployRequest.create_at.__gt__(human_date())):
             data[req.deploy.app_id]['count'] += 1
-        print(data,'!!!!!!!!!!!!!!!====')
+        data = sorted(data.values(), key=lambda x: x['count'], reverse=True)[:5]
+        return self.jsonify(data)
 
-        return self.jsonify()
+class GetDeploy(APIView):
+    '''
+        应用部署
+    '''
+    url_prefix = '/deploy'
+
+    def get(self):
+        host = Host.query.filter(Host.deleted_at.is_(None)).count()
+        data = { x.id: {'name': x.name, 'count': 0} for x in App.objects.all() }
+        for dep in Deploy.query.all():
+            data[dep.app_id]['count'] += len(json.loads(dep.host_ids))
+        data = filter(lambda x: x['count'], data.values())
+        return self.jsonify({'host': host, 'res': list(data)})
