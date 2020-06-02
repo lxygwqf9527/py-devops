17477b96ca3a94c7c9db56affc541ab7a786b37f
Administrator <admin@example.com>
2020-06-02 15:12:54 +0400

    home get deploy2

--- api/views/home/views.py
+++ api/views/home/views.py
@@ -44,6 +44,8 @@ class GetRequest(APIView):
     '''
         deploy发布展示
     '''
+    url_prefix = '/request'
+    
     data = { x.id: {'name': x.name, 'count': 0 } for x in App.query.all() }
     print(data,'=-=-=-=-=-==-=-=-=-=-')
     for req in DeployRequest.query.filter(DeployRequest.create_at.__gt__(human_date)):
