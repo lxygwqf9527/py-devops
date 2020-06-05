ac96d19fe014e86d77d99d223490f98087e6619a
Administrator <admin@example.com>
2020-06-05 12:02:33 +0400

    host valid_ssh24

--- api/views/host/host.py
+++ api/views/host/host.py
@@ -20,7 +20,7 @@ class HostView(APIView):
             获取所有的zones和主机
         '''
         hosts = Host.query.filter(Host.deleted_at.is_(None)).all()
-        zones = [x['zone'] for x in [ i for i in hosts if i.zone ]
+        zones = [x for x in [ i for i in hosts if i.zone ]]
         print(zones)
         return self.jsonify({'zones': zones, 'hosts': [x.to_dict() for x in hosts]})
     
