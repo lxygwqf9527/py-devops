5598fd77b8b678f3aa29e7aed77fd0f66db5ebbe
Administrator <admin@example.com>
2020-06-05 11:53:26 +0400

    host valid_ssh18

--- api/views/host/host.py
+++ api/views/host/host.py
@@ -20,7 +20,8 @@ class HostView(APIView):
             获取所有的zones和主机
         '''
         hosts = Host.query.filter(Host.deleted_at.is_(None))
-        zones = [x['zone'] for x in hosts.order_by(Host.zone).with_entities(Host.zone).distinct()]
+        print(hosts,'======================')
+        #zones = [x['zone'] for x in hosts.order_by(Host.zone).with_entities(Host.zone).distinct()]
         return self.jsonify({'zones': zones, 'hosts': [x.to_dict() for x in hosts]})
     
     def post(self):
@@ -41,7 +42,6 @@ class HostView(APIView):
             Host.get_by(id=id, to_dict=False).update(request.values)
         else:
             request.values['created_by'] = g.user.id
-            print(request.values,'====================')
             Host.create(**request.values)
         return self.jsonify(error='')
 
