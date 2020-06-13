28b5aa3096f8083daac7e441d69f22e23dc37b17
Administrator <admin@example.com>
2020-06-13 11:01:33 +0400

    host post11

--- api/views/host/host.py
+++ api/views/host/host.py
@@ -44,7 +44,7 @@ class HostView(APIView):
 
         if id:
             Host.get_by(id=id, to_dict=False).update(request.values)
-        elif Host.query.filter_by(db.exists().where(Host.name==name,Host.deleted_by.is_(None))).scalar():
+        elif db.query.filter_by(db.exists().where(Host.name==name,Host.deleted_by.is_(None))).scalar():
             return self.jsonify(erro='已存在的主机名称【{name}】')
         else:
             request.values['created_by'] = g.user.id
