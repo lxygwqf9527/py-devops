305f0c4f1e41806a82317fbb842fdd52b50ec189
Administrator <admin@example.com>
2020-06-14 17:21:09 +0400

    host post40

--- api/views/host/host.py
+++ api/views/host/host.py
@@ -59,7 +59,8 @@ class HostView(APIView):
             删除主机
         '''
         data = AttrDict(id=request.values['id'])
-        deploy = Deploy.query.filter(Deploy.host_ids.op('regexp')(fr'\D{data.id}\D')).first()
+
+        deploy = Deploy.query.filter(Deploy.host_ids.op('regexp')(r'\D{data.id}\D')).first()
         print(deploy,'=========================$$$$$$$$$$$$$$$')
         #for deploy in Deploy.query.all():
         #    if int(request.values['id']) in eval(deploy.host_ids):
