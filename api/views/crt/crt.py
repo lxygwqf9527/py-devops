d267f0b4eac772efd5c380513e1c2dc5c5a6fd47
faker <faker.com>
2020-08-04 15:33:07 +0800

    add ssl_type model3

--- api/views/crt/crt.py
+++ api/views/crt/crt.py
@@ -18,13 +18,13 @@ class SSLView(APIView):
         #     if not g.user.has_ssl_perm(ssl_id):
         #         return self.jsonify(error='无权访问该ssl证书')
         ssls = SSL.query.filter(SSL.deleted_at.is_(None)).all()
-        ssl_types = [i.ssl_type for i in ssls if i.ssl_type ]
+        ssl_types = [i.ssl_type.name for i in ssls if i.ssl_type ]
         perms = [x.id for x in ssls] if g.user.is_supper else g.user.ssl_perms
         # 这里证书字符串和证书私钥也一起提交给了前端，证书太多可能会导致前端数据加载太慢，卡顿的现象出现,后面再优化
         print({'ssl_types': ssl_types, 'ssls': [x.to_dict() for x in ssls], 'perms': perms})
         return self.jsonify({'ssl_types': ssl_types, 'ssls': [x.to_dict() for x in ssls], 'perms': perms})
 
-    def post(self):
-        '''
+    # def post(self):
+    #     '''
 
-        '''
+    #     '''
