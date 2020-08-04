4472b4a99780613ac70800d4e9d51bed545d32ea
faker <faker.com>
2020-08-04 15:47:36 +0800

    add ssl_type model5

--- api/views/crt/crt.py
+++ api/views/crt/crt.py
@@ -21,8 +21,14 @@ class SSLView(APIView):
         ssl_types = [i.ssl_type.name for i in ssls if i.ssl_type ]
         perms = [x.id for x in ssls] if g.user.is_supper else g.user.ssl_perms
         # 这里证书字符串和证书私钥也一起提交给了前端，证书太多可能会导致前端数据加载太慢，卡顿的现象出现,后面再优化
-        print({'ssl_types': ssl_types, 'ssls': [x.to_dict() for x in ssls], 'perms': perms})
-        return self.jsonify({'ssl_types': ssl_types, 'ssls': [x.to_dict() for x in ssls], 'perms': perms})
+        ssl_list = []
+        for x in ssls:
+            if x.ssl_type_id:
+                type_name = x.ssl_type.name
+            x = x.to_dict()
+            x['ssl_type'] = type_name
+            ssl_list.append(x)
+        return self.jsonify({'ssl_types': ssl_types, 'ssls': ssl_list, 'perms': perms})
 
     # def post(self):
     #     '''
