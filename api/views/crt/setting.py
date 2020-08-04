dd5dd6db757ccd882eda50fe16879870c6aa9b96
faker <faker.com>
2020-08-04 23:55:01 +0800

    add ssl setting7

--- api/views/crt/setting.py
+++ api/views/crt/setting.py
@@ -13,9 +13,10 @@ class SSLSettingView(APIView):
     def get(self):
         data = []
         for i in SSLSetting.query.all():
+            res = i.to_dict()
             if i.ssl_type:
-                i['ssl_type_key'] = i.ssl_type.key
-                i['ssl_type_value'] = i.ssl_type.value
-            data.append(i)
+                res['ssl_type_key'] = i.ssl_type.key
+                res['ssl_type_value'] = i.ssl_type.value
+            data.append(res)
         
         return self.jsonify(data)
