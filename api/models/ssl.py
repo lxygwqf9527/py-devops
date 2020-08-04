4938e86d5bf37019d34e2ab1cbbbbe3dfddccb15
faker <faker.com>
2020-08-04 20:34:41 +0800

    add ssl setting1

--- api/models/ssl.py
+++ api/models/ssl.py
@@ -8,10 +8,11 @@ from api.libs import human_datetime
 class SSLType(Model):
     __tablename__ = 'ssl_type'
 
-    name = db.Column(db.String(20),nullable=True)
+    key = db.Column(db.String(20),nullable=True)
+    value = db.Column(db.Text)
 
     def __str__(self):
-        return '<SSLType %r>' % self.name
+        return '<SSLType %r>' % self.key
     
 class SSL(Model):
     __tablename__ = 'ssls'
@@ -29,3 +30,16 @@ class SSL(Model):
 
     def __str__(self):
         return '<SSL %r>' % self.name
+
+class SSLSetting(Model):
+    __tablename__ = 'ssl_setting'
+
+    key = db.Column(db.String(50), unique=True)
+    value = db.Column(db.Text)
+    desc = db.Column(db.String(255), nullable=True)
+
+    ssl_type_id = db.Column(db.Integer, db.ForeignKey('ssl_type.id'))
+    ssl_type = db.relationship('SSLType', backref=db.backref('ssl_type'), lazy='subquery', foreign_keys=[ssl_type_id])
+
+    def __str__(self):
+        return '<Setting %r>' % self.key
