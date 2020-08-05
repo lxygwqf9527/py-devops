b5c7e5e5c12559d42e6b1d20fa7016d52821e78b
faker <faker.com>
2020-08-05 19:20:00 +0800

    add ssh8

--- api/views/host/host.py
+++ api/views/host/host.py
@@ -21,7 +21,7 @@ class HostView(APIView):
         '''
             获取所有的zones和主机
         '''
-        host_id = request.values.get('id') or host_id
+        host_id = request.values.get('id')
         if host_id:
             if not g.user.has_host_perm(host_id):
                 return self.jsonify(error='无权访问该主机')
