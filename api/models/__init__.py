fb2490a88a9841f710fbeb5cfeada4353c644efc
faker <faker.com>
2020-08-07 19:56:41 +0800

    add ssl patch6

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,13 +1,1 @@
 # -*- coding:utf-8 -*- 
-
-from .account import User, Role
-from .notify import Notify
-from .app import App, Deploy, DeployExtends, DeployExtend1, DeployExtend2
-from .config import Environment, Service, Config, ConfigHistory, ConfigType, ConfigHistoryAction
-from .settings import Setting
-from .host import Host
-from .scheduler import Task
-from .monitor import Detection
-from .alarm import AlarmStatus, AlarmGroup, AlarmContact, Alarm
-from .deploy import DeployRequestType, DeployRequestStatus, DeployRequest
-from .ssl import SSL, SSLSetting
