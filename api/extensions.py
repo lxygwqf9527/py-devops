54b1af1d564593038007e9b534ccde64f947a9f6
faker <faker.com>
2020-08-07 19:47:25 +0800

    add ssl patch3

--- api/extensions.py
+++ api/extensions.py
@@ -10,8 +10,53 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 # from api.lib.utils import ESHandler
-from api.libs import RedisHandler
 
+
+# other
+class RedisHandler(object):
+    def __init__(self, flask_app=None):
+        self.flask_app = flask_app
+        self.r = None
+
+    def init_app(self, app):
+        self.flask_app = app
+        config = self.flask_app.config
+        try:
+            pool = redis.ConnectionPool(
+                max_connections=config.get("REDIS_MAX_CONN"),
+                host=config.get("CACHE_REDIS_HOST"),
+                port=config.get("CACHE_REDIS_PORT"),
+                db=config.get("REDIS_DB"))
+            self.r = redis.Redis(connection_pool=pool)
+        except Exception as e:
+            current_app.logger.warning(str(e))
+            current_app.logger.error("init redis connection failed")
+
+    def get(self, key_ids, prefix):
+        try:
+            value = self.r.hmget(prefix, key_ids)
+        except Exception as e:
+            current_app.logger.error("get redis error, {0}".format(str(e)))
+            return
+        return value
+
+    def _set(self, obj, prefix):
+        try:
+            self.r.hmset(prefix, obj)
+        except Exception as e:
+            current_app.logger.error("set redis error, {0}".format(str(e)))
+
+    def create_or_update(self, obj, prefix):
+        self._set(obj, prefix)
+
+    def delete(self, key_id, prefix):
+        try:
+            ret = self.r.hdel(prefix, key_id)
+            if not ret:
+                current_app.logger.warn("[{0}] is not in redis".format(key_id))
+        except Exception as e:
+            current_app.logger.error("delete redis key error, {0}".format(str(e)))
+    
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 migrate = Migrate()
@@ -26,3 +71,4 @@ socketio = SocketIO()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'LoginView'
+
