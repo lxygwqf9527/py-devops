3cd3ef9102ef3bbb6909b9a2d563ad43d93b20e1
faker <faker.com>
2020-08-07 21:23:37 +0800

    add ssl patch22

--- api/extensions.py
+++ api/extensions.py
@@ -10,6 +10,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+import redis
 # from api.lib.utils import ESHandler
 
 
