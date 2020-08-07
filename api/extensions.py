c82d4871ba9d2fb1e9d9f967ef8cf54ff5e039fb
faker <faker.com>
2020-08-07 20:52:51 +0800

    add ssl patch16

--- api/extensions.py
+++ api/extensions.py
@@ -5,7 +5,7 @@ from celery import Celery
 from flask_bcrypt import Bcrypt
 from flask_caching import Cache
 from flask_cors import CORS
-from flask_login import LoginManager
+from flask_login import LoginManager, current_app
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
