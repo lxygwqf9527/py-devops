f02b3175db7331c34d062c031368bd5fb0090771
faker <faker.com>
2020-08-11 23:08:38 +0800

    add acme_install27

--- api/views/crt/acme.py
+++ api/views/crt/acme.py
@@ -9,7 +9,9 @@ from api.resource import APIView
 @celery.task(name="ssl.acme.install", queue="acme_install")
 def acme_install_task(host_ids):
     hosts = Host.get_by_in_id(host_ids)
-    print(hosts,'----------')
+    private_key = AppSetting.get('private_key')
+    cli = SSH(hostname, port, username, private_key)
+    code, out = cli.exec_command('echo 1>>/opt/a')
 
 class AcmeInstall(APIView):
     url_prefix = ('/acme/install','/acme/status')
