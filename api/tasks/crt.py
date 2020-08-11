1ad735e2eadf5c4da8612dfa4246e9d287d180b7
faker <faker.com>
2020-08-11 23:59:16 +0800

    add acme_install36

--- api/tasks/crt.py
+++ api/tasks/crt.py
@@ -1,16 +1,13 @@
 # -*- coding:utf-8 -*-
 from flask import current_app
 
-from autoapp import app
 from api.extensions import celery
-
 
 @celery.task(name="ssl.acme.install", queue="acme_install")
 def acme_install_task(host_ids):
-    with app.app_context():
-        hosts = Host.get_by_in_id(host_ids)
-        private_key = AppSetting.get('private_key')
-        for i in hosts:
-            cli = SSH(hosts.hostname, hosts.port, hosts.username, private_key)
-            code, out = cli.exec_command('echo 1>>/opt/a')
-            print(out,'===========')
+    hosts = Host.get_by_in_id(host_ids)
+    private_key = AppSetting.get('private_key')
+    for i in hosts:
+        cli = SSH(hosts.hostname, hosts.port, hosts.username, private_key)
+        code, out = cli.exec_command('echo 1>>/opt/a')
+        print(out,'===========')
