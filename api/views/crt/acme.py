600746377b595637a707c07be4c102a0ec3acc31
faker <faker.com>
2020-08-11 21:28:55 +0800

    add acme_install21

--- api/views/crt/acme.py
+++ api/views/crt/acme.py
@@ -1,6 +1,6 @@
 from flask import request
 
-from flask import current_app
+from flask import g
 from api.models import Host
 from api.extensions import celery
 from api.libs.ssh import SSH
@@ -8,7 +8,7 @@ from api.resource import APIView
 
 @celery.task
 def acme_install_task(host_ids):
-    with current_app.app_context():
+    with g.app..app_context():
         hosts = Host.get_by_in_id(host_ids)
         print(hosts,'----------')
 
