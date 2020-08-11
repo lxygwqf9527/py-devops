6ad5ed2f73be52530b26ca5b8fadeaa8ab110820
faker <faker.com>
2020-08-11 21:43:11 +0800

    add acme_install25

--- autoapp.py
+++ autoapp.py
@@ -3,10 +3,10 @@
 from flask import g
 from flask_login import current_user
 from api.app import create_app
+from api.extensions import celery
 
 app = create_app()
 app.app_context().push()
 @app.before_request
 def after_request():
     g.user = current_user
-    g.app = app
