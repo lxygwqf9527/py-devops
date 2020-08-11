bf08d671d69dce4da353ab62a158d13aa7e71328
faker <faker.com>
2020-08-11 21:40:10 +0800

    add acme_install23

--- autoapp.py
+++ autoapp.py
@@ -5,7 +5,7 @@ from flask_login import current_user
 from api.app import create_app
 
 app = create_app()
-
+app.app_context().push()
 @app.before_request
 def after_request():
     g.user = current_user
---
+++ celery_worker.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+from api.app import create_app
+from api.extensions import celery
+
+# celery worker -A celery_worker.celery -l DEBUG -E -Q <queue_name> --concurrency=1
+
+app = create_app()
+app.app_context().push()
