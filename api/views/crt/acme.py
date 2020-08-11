e17aa8e4c5b998bd500466b1c2d9ba6dee8baff0
faker <faker.com>
2020-08-11 14:36:10 +0800

    add acme install1

--- api/views/crt/acme.py
+++ api/views/crt/acme.py
@@ -1,12 +1,27 @@
 from flask import request
 
+from api import app
+from api.models import Host
+from api.extensions import celery
+from api.libs.ssh import SSH
 from api.resource import APIView
 
+@celery.task
+def acme_install_task(host_ids):
+    with app.app_context():
+        hosts = Host.get_by_in_id(host_ids)
+        print(hosts,'----------')
+
 class AcmeInstall(APIView):
-    url_prefix = '/acme/install'
+    url_prefix = ('/acme/install','/acme/status')
     
     def get(self):
-        print(request.values)
+        if request.values.get('id'):
+            print(request.values)
     
     def post(self):
-        print(request.values)
+        
+        acme_install_task.delay(request.values('host_ids'))
+
+        return self.jsonify(error="")
+
