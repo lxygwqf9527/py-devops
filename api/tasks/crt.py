71264f0dc568e7901651bde0ff7dafc577349d5f
faker <faker.com>
2020-08-12 14:16:20 +0800

    add acme_install44

--- api/tasks/crt.py
+++ api/tasks/crt.py
@@ -3,13 +3,13 @@ from flask import current_app
 
 from api.extensions import celery
 from api.models.host import Host
-from api.libs.utils import AppSetting
+from api.utils import AppSetting
 
-@celery.task
+@celery.task(name="ssl.acme.install", queue="acme_install")
 def acme_install_task(host_ids):
     hosts = Host.get_by_in_id(host_ids)
     private_key = AppSetting.get('private_key')
     for i in hosts:
         cli = SSH(hosts.hostname, hosts.port, hosts.username, private_key)
         code, out = cli.exec_command('echo 1>>/opt/a')
-        print(code, out,'===========')
+        print(out,'===========')
--- celery_worker.py
+++ celery_worker.py
@@ -3,7 +3,7 @@
 from api.app import create_app
 from api.extensions import celery
 
-# celery worker -A celery_worker.celery -l DEBUG -E -Q <queue_name> --concurrency=1
+# celery worker -A celery_worker.celery -l DEBUG -E -Q devops_queue --concurrency=1
 
 app = create_app()
 app.app_context().push()
