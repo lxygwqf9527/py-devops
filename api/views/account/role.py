3bc37c9c848333af1b3da5ace7db288e1059bccc
Your Name <you@example.com>
2020-11-11 03:01:19 +0800

    2020_1111_11

--- api/views/account/role.py
+++ api/views/account/role.py
@@ -2,11 +2,13 @@
 
 from api.models.account import Role
 from api.resource import APIView
+from api.libs.utils import AttrDict
 
 class RoleView(APIView):
     url_prefix = "/role"
 
     def get(self):
         roles = Role.query.all()
+        roles = AttrDict(roles=roles)
         print(roles)
         return self.jsonify(error="")
