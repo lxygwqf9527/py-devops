35647dbb2bdc1eee7557ab4aeb2b15a7f5d3b021
Your Name <you@example.com>
2020-11-13 20:16:15 +0800

    2020_1113_1

--- api/views/account/role.py
+++ api/views/account/role.py
@@ -3,6 +3,7 @@
 from api.models.account import Role
 from api.resource import APIView
 from api.libs.utils import AttrDict
+from flask import request
 
 class RoleView(APIView):
     url_prefix = "/role"
@@ -10,3 +11,6 @@ class RoleView(APIView):
     def get(self):
         roles = [i.to_dict() for i in Role.query.all()]
         return self.jsonify(roles)
+    
+    def patch(self):
+        print(request.values)
